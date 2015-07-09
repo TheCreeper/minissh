d1ba6c0f6ac596603befc225bc4c53db9cf7010c
TheCreeper <dc0@firebit.co.uk>
2015-07-09 15:20:09 +0100

    Added descriptions to minissh-keygen and ability to encrypt newly generated private keys.

--- cmd/minissh-keygen/main.go
+++ cmd/minissh-keygen/main.go
@@ -14,48 +14,48 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-const (
-	PubKeyPostfix = ".pub"
-)
-
 var (
 	KeySize    int
 	KeyType    string
-	Passphrase string
+	Password   string
 	Comment    string
 	OutputFile string
 )
 
 func init() {
-	flag.IntVar(&KeySize, "b", 256, "")
-	flag.StringVar(&KeyType, "t", "ecdsa", "")
-	flag.StringVar(&Passphrase, "p", "", "")
-	flag.StringVar(&OutputFile, "f", "", "")
+	flag.IntVar(&KeySize, "b", 256, "Specifies the number of bits in the key to create. For ecdsa keys the minimum is 256 bits and the maximum is 521 bits.")
+	flag.StringVar(&KeyType, "t", "ecdsa", "Specifies the type of key to create. Only option is ecdsa.")
+	flag.StringVar(&Password, "P", "", "The password used to encrypt the private key. AES-128 is used.")
+	flag.StringVar(&OutputFile, "f", os.ExpandEnv("$HOME/.ssh/id_ecdsa"), "The directory where the private/public keypair files will be written.")
 	flag.Parse()
-
-	if len(OutputFile) == 0 {
-		OutputFile = os.ExpandEnv("$HOME/.ssh/id_ecdsa")
-	}
 }
 
 func main() {
-	public, private, err := GenerateECDSAKeyPair(KeySize)
+	public, private, err := GenerateKeyPair(KeyType, KeySize, Password)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	if err = ioutil.WriteFile(os.ExpandEnv(OutputFile), private, 0600); err != nil {
+	if err := ioutil.WriteFile(os.ExpandEnv(OutputFile), private, 0600); err != nil {
 		log.Fatal(err)
 	}
 
-	if err = ioutil.WriteFile(os.ExpandEnv(OutputFile+PubKeyPostfix), public, 0600); err != nil {
+	if err := ioutil.WriteFile(os.ExpandEnv(OutputFile+".pub"), public, 0640); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func GenerateECDSAKeyPair(size int) (public, private []byte, err error) {
+func GenerateKeyPair(keytype string, keysize int, password string) (public, private []byte, err error) {
+	// Add more key types in future
+	switch keytype {
+	case "ecdsa":
+		return GenerateECDSAKeyPair(keysize, password)
+	}
+	return
+}
+
+func GenerateECDSAKeyPair(keysize int, password string) (public, private []byte, err error) {
 	var curve elliptic.Curve
-	switch size {
+	switch keysize {
 	case 256:
 		curve = elliptic.P256()
 	case 384:
@@ -72,15 +72,6 @@ func GenerateECDSAKeyPair(size int) (public, private []byte, err error) {
 		return
 	}
 
-	// Marshal the private key
-	prvKeyDer, err := x509.MarshalECPrivateKey(prvKey)
-	if err != nil {
-		return
-	}
-
-	prvKeyBlock := &pem.Block{Type: "EC PRIVATE KEY", Bytes: prvKeyDer}
-	private = pem.EncodeToMemory(prvKeyBlock)
-
 	// Marshal the public key
 	sshPubKey, err := ssh.NewPublicKey(&prvKey.PublicKey)
 	if err != nil {
@@ -88,5 +79,26 @@ func GenerateECDSAKeyPair(size int) (public, private []byte, err error) {
 	}
 	public = ssh.MarshalAuthorizedKey(sshPubKey)
 
+	// Marshal the private key
+	prvKeyDer, err := x509.MarshalECPrivateKey(prvKey)
+	if err != nil {
+		return
+	}
+	block := &pem.Block{Type: "EC PRIVATE KEY", Bytes: prvKeyDer}
+
+	// Encrypt the private key
+	if len(password) != 0 {
+		// AES128 is the only option for private key encryption just like in ssh-keygen.
+		block, err = x509.EncryptPEMBlock(rand.Reader,
+			"EC PRIVATE KEY",
+			prvKeyDer,
+			[]byte(password),
+			x509.PEMCipherAES128)
+		if err != nil {
+			return
+		}
+	}
+
+	private = pem.EncodeToMemory(block)
 	return
 }
